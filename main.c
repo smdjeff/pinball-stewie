c81fdff3f144293a48c7bbac4eda877eaddc7f08
Jeff <smdjeff@gmail.com>
2020-04-27 06:09:10 +0100

    get leds working

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 // http://abyz.me.uk/rpi/pigpio/
 #include <pigpio.h>
 
-// our led matrix "ram"
+// our led matrix memory map
 uint8_t col_a_row_0 = 0x00;
 uint8_t col_a_row_1 = 0x00;
 uint8_t col_b_row_0 = 0x00;
@@ -19,17 +20,17 @@ uint8_t col_c_row_1 = 0x00;
 
 // based on studying the family guy pinball schematics page 142,
 // we see the shift register led matrix works like this
-#define CHRIS_C(on)  (on) ? BITON(col_a_row_0,4) : BITOFF(col_a_row_0,4)
-#define CHRIS_H(on)  (on) ? BITON(col_a_row_0,3) : BITOFF(col_a_row_0,3)
-#define CHRIS_R(on)  (on) ? BITON(col_a_row_0,2) : BITOFF(col_a_row_0,2)
-#define CHRIS_I(on)  (on) ? BITON(col_a_row_0,1) : BITOFF(col_a_row_0,1)
-#define CHRIS_S(on)  (on) ? BITON(col_a_row_0,0) : BITOFF(col_a_row_0,0)
+#define CHRIS_C(on)  (on) ? BITON(col_c_row_1,3) : BITOFF(col_c_row_1,3)
+#define CHRIS_H(on)  (on) ? BITON(col_c_row_1,2) : BITOFF(col_c_row_1,2)
+#define CHRIS_R(on)  (on) ? BITON(col_c_row_1,1) : BITOFF(col_c_row_1,1)
+#define CHRIS_I(on)  (on) ? BITON(col_c_row_1,0) : BITOFF(col_c_row_1,0)
+#define CHRIS_S(on)  (on) ? BITON(col_a_row_1,0) : BITOFF(col_a_row_1,0)
 
 #define MEG_M(on)    (on) ? BITON(col_a_row_1,1) : BITOFF(col_a_row_1,1)
 #define MEG_E(on)    (on) ? BITON(col_a_row_1,2) : BITOFF(col_a_row_1,2)
 #define MEG_G(on)    (on) ? BITON(col_a_row_1,3) : BITOFF(col_a_row_1,3)
 
-#define PETER_P(on)  (on) ? BITON(col_b_row_0,0) : BITOFF(col_b_row_0,0)
+#define PETER_P(on)  (on) ? BITON(col_b_row_0,1) : BITOFF(col_b_row_0,1)
 #define PETER_E(on)  (on) ? BITON(col_b_row_1,0) : BITOFF(col_b_row_1,0)
 #define PETER_T(on)  (on) ? BITON(col_b_row_1,1) : BITOFF(col_b_row_1,1)
 #define PETER_EE(on) (on) ? BITON(col_b_row_1,2) : BITOFF(col_b_row_1,2)
@@ -40,11 +41,11 @@ uint8_t col_c_row_1 = 0x00;
 #define LOIS_I(on)   (on) ? BITON(col_c_row_0,1) : BITOFF(col_c_row_0,1)
 #define LOIS_S(on)   (on) ? BITON(col_c_row_0,0) : BITOFF(col_c_row_0,0)
 
-#define BRIAN_B(on)  (on) ? BITON(col_c_row_1,3) : BITOFF(col_c_row_1,3)
-#define BRIAN_R(on)  (on) ? BITON(col_c_row_1,2) : BITOFF(col_c_row_1,2)
-#define BRIAN_I(on)  (on) ? BITON(col_c_row_1,1) : BITOFF(col_c_row_1,1)
-#define BRIAN_A(on)  (on) ? BITON(col_c_row_1,0) : BITOFF(col_c_row_1,0)
-#define BRIAN_N(on)  (on) ? BITON(col_a_row_1,0) : BITOFF(col_a_row_1,0)
+#define BRIAN_B(on)  (on) ? BITON(col_a_row_0,4) : BITOFF(col_a_row_0,4)
+#define BRIAN_R(on)  (on) ? BITON(col_a_row_0,3) : BITOFF(col_a_row_0,3)
+#define BRIAN_I(on)  (on) ? BITON(col_a_row_0,2) : BITOFF(col_a_row_0,2)
+#define BRIAN_A(on)  (on) ? BITON(col_a_row_0,1) : BITOFF(col_a_row_0,1)
+#define BRIAN_N(on)  (on) ? BITON(col_a_row_0,0) : BITOFF(col_a_row_0,0)
 
 // convenience functions for applying a bitmask to the letters of a 
 // name string of lights
@@ -85,7 +86,7 @@ void brian(int value) {
     BRIAN_N( (value & 0x10) );
 }
 
-#define delay_ms(ms)    do { printf("delay_ms:%x\n",ms); usleep(ms*1000); } while(0)
+#define delay_ms(ms)    do { usleep(ms*1000); } while(0)
 
 // arbitrary connections of the stewie pinball machine's J1 connector to our
 // raspberry pi gpios
@@ -101,6 +102,7 @@ void brian(int value) {
 
 
 void ledwrite(uint8_t b) {
+    gpioWrite( LED_MATRIX_CLK, 0 );
     gpioWrite( LED_MATRIX_D0, (b&0x01)?1:0 );
     gpioWrite( LED_MATRIX_D1, (b&0x02)?1:0 );
     gpioWrite( LED_MATRIX_D2, (b&0x04)?1:0 );
@@ -109,12 +111,15 @@ void ledwrite(uint8_t b) {
     gpioWrite( LED_MATRIX_D5, (b&0x20)?1:0 );
     gpioWrite( LED_MATRIX_D6, (b&0x40)?1:0 );
     gpioWrite( LED_MATRIX_D7, (b&0x80)?1:0 );
-    delay_ms( 1 );
     gpioWrite( LED_MATRIX_CLK, 1 );
-    delay_ms( 1 );
-    gpioWrite( LED_MATRIX_CLK, 0 );
-    delay_ms( 1 );
 }
+
+uint64_t time_ms(void) {
+    struct timeval tv;
+    gettimeofday( &tv,NULL );
+    return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+}
+
 
 int main (void) {
 
@@ -128,24 +133,39 @@ int main (void) {
     gpioSetMode( LED_MATRIX_D5, PI_OUTPUT );
     gpioSetMode( LED_MATRIX_D6, PI_OUTPUT );
     gpioSetMode( LED_MATRIX_D7, PI_OUTPUT );
-    
-    brian( 0x1F ); // "BRIAN" all characters lit
 
     for (;;) {
-      
-        // drive the led matrix
-        ledwrite( 0x81 ); // row 0 latch
-        ledwrite( col_c_row_0 );
-        ledwrite( col_b_row_0 );
-        ledwrite( col_a_row_0 );
-        delay_ms( 10 ); // hold
 
-        ledwrite( 0x82 ); // row 1 latch
+        uint64_t t = time_ms();
+        static uint64_t last_t = 0;
+	if ( t - last_t > 100 ) {
+            last_t = t;
+            static uint8_t ct = 1;
+            brian( ct );
+            meg( ct );
+            peter( ct );
+            lois( ct );
+            chris( ct );
+            ct <<= 1;
+            if ( ct > 0x1f ) {
+                ct = 1;
+            }
+        } 
+
+        // drive the led matrix
+        ledwrite( 0x82 ); // row 0 latch
+        ledwrite( col_c_row_0);
+        ledwrite( col_b_row_0);
+        ledwrite( col_a_row_0);
+        delay_ms( 8 ); //hold
+
+        ledwrite( 0x81 ); // row 1 latch
         ledwrite( col_c_row_1 );
         ledwrite( col_b_row_1 );
         ledwrite( col_a_row_1 );
-        delay_ms( 10 ); // hold
+        delay_ms( 8 ); // hold
     }
     
     return 0;
 }
+
