b678172fe2224145169b8be4c6bafe8231fb8d96
Jeff Mathews <smdjeff@gmail.com>
2021-08-01 15:36:20 -0600

    couple scripts

---
+++ sounds/download.sh
@@ -0,0 +1,6 @@
+
+wget https://raw.githubusercontent.com/circulosmeos/gdown.pl/master/gdown.pl
+chmod +x *.pl
+./gdown.pl https://drive.google.com/file/d/1Z2kLpOeDKzqBpQP47zslXBqrTpA0zDV_
+./gdown.pl https://drive.google.com/file/d/1S2_zKztEICtA-cRrjxmtiKpKuOaNHaBl
+unzip 'gdown*'
