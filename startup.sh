a2f01587eaaa344ddf4ad1f0fa7ad51f03254796
Jeff Mathews <smdjeff@gmail.com>
2021-08-03 10:43:12 -0600

    move from init.d to systemd services

--- startup.sh
+++
@@ -1,9 +0,0 @@
-### call me from /etc/rc.local
-### sudo bash /home/pi/startup.sh &
-
-export STEWIE_PATH="/home/pi/git/stewie-pinball"
-amixer set PCM 75%
-$STEWIE_PATH/display-server.py &
-sleep 1 && $STEWIE_PATH/stewie &
-exit 0
-
