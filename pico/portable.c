44e8ff6e613845f6518897444b60ceb10a5b01ec
Jeff Mathews <smdjeff@gmail.com>
2021-10-24 11:06:34 -0600

    pigpio interface for pico. i2c, pwm, gpio glitch filter,  timers,alerts.

--- pico/portable.c
+++ pico/portable.c
@@ -8,27 +8,126 @@
 
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/i2c.h"
+#include "hardware/sync.h"
+#include "hardware/watchdog.h"
 
 #include "../portable.h"
+
+// portability layer, making the raspberrypi pico fit the pigpio api from regular pi
+
+static int64_t halt_alarm(alarm_id_t id, void *user_data) {
+  watchdog_enable(100,false);
+  for (;;) { __wfi(); };
+}
+
+void haltPico(void) {
+  // delay for stdio flush
+  add_alarm_in_ms( 1000, halt_alarm, 0/*user_data*/, true/*fire_if_past*/ );
+}
     
 int usleep(useconds_t usec) {
-    sleep_us(usec);
+    sleep_us( usec );
 }
 
 int gpioInitialise(void) { 
+    stdio_init_all();
+    usleep(1000* 5000);
+    printf("pico booted\n");
     return 0; 
 }
 
 int gpioSetMode(unsigned gpio, unsigned mode) { 
+    gpio_init( gpio );
+    gpio_set_dir( gpio, mode );
     return 0; 
+}
+
+static struct repeating_timer timers[10] = {0,};
+
+static bool repeating_timer_callback(struct repeating_timer *t) {
+    // extra call accounts for differing callback prototype
+    assert( t );
+    gpioTimerFunc_t f = t->user_data;
+    assert( f );
+    f( );
+    return true;
 }
 
 int gpioSetTimerFunc(unsigned timer, unsigned millis, gpioTimerFunc_t f) {
-    return 0; 
+    bool success = false;
+    assert( timer < sizeof(timers)/sizeof(timers[0]) );
+    if ( millis ) {
+      success = add_repeating_timer_ms( millis, repeating_timer_callback, f/*userdata*/, &timers[timer] );
+      printf("add_repeating_timer_ms:%d %d\n", success, timers->alarm_id);
+    } else {
+      success = cancel_repeating_timer( &timers[timer] );
+    }
+    return (success == true)? 0 : 1; // error = 0 == success
 }
 
-int gpioSetAlertFunc(unsigned user_gpio, gpioAlertFunc_t f) {
-    return 0; 
+typedef struct {
+    alarm_id_t id;
+    uint32_t events;
+    uint steady;
+    uint gpio;
+    gpioAlertFunc_t callback;
+} glitch_t;
+static glitch_t glitches[32] = {0,};
+
+#define PICO_GPIO_EVENT_LEVEL_LOW   (1<<0)
+#define PICO_GPIO_EVENT_LEVEL_HIGH  (1<<1)
+#define PICO_GPIO_EVENT_EDGE_LOW    (1<<2)
+#define PICO_GPIO_EVENT_EDGE_HIGH   (1<<3)
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    glitch_t* glitch = user_data;
+    glitch->id = 0;
+    if ( glitch->callback ) {
+        int level = -1;
+        if ( glitch->events & PICO_GPIO_EVENT_EDGE_HIGH ) {
+            if ( gpio_get( glitch->gpio ) != 0 ) {
+                level = RISING_EDGE;
+            }
+        }
+        if ( glitch->events & PICO_GPIO_EVENT_EDGE_LOW ) {
+            if ( gpio_get( glitch->gpio ) == 0 ) {
+                level = FALLING_EDGE;
+            }
+        }
+        glitch->callback( glitch->gpio, level, 0/*tick*/ );
+    }
+    return 0; //no auto-reschedule
+}
+
+void gpio_callback(uint gpio, uint32_t events) {
+    glitch_t* glitch = &glitches[gpio];
+    glitch->events = events;
+    if ( glitch->id > 0 ) {
+        bool success = cancel_alarm( glitch->id );
+        assert( success );
+    }
+    glitch->id = add_alarm_in_us( glitch->steady, alarm_callback, &glitches[gpio]/*user_data*/, true/*fire_if_past*/ );
+    assert ( glitch->id > 0 );
+}
+
+int gpioSetAlertFunc(unsigned gpio, gpioAlertFunc_t f) {
+    glitch_t* glitch = &glitches[gpio];
+    if ( f ) {
+        glitch->callback = f;
+        glitch->gpio = gpio;
+        gpio_set_irq_enabled_with_callback( gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true/*enable*/, &gpio_callback);
+    } else {
+        memset( glitch, 0x00, sizeof(glitch_t) );
+    }
+    return 0;
+}
+
+int gpioGlitchFilter(unsigned gpio, unsigned steady) {
+    glitch_t* glitch = &glitches[gpio];
+    glitch->steady = steady;
+    return 0;
 }
 
 void* gpioStartThread(gpioThreadFunc_t f, void *userdata) {
@@ -36,22 +135,40 @@ void* gpioStartThread(gpioThreadFunc_t f, void *userdata) {
 }
 
 int gpioWrite(unsigned gpio, unsigned level) {
+    gpio_set_function( gpio, GPIO_FUNC_SIO );
+    gpio_put( gpio, level );
     return 0; 
 }
 
-int gpioRead(unsigned gpio) { 
-    return 0;
+int gpioRead(unsigned gpio) {
+    return gpio_get( gpio );
 }
 
 int gpioSetPullUpDown(unsigned gpio, unsigned pud) { 
+    switch ( pud ) {
+      case PI_PUD_OFF:
+        gpio_disable_pulls( gpio ); 
+        break;
+      case PI_PUD_DOWN:
+        gpio_pull_down( gpio );
+        break;
+      case PI_PUD_UP:
+        gpio_pull_up( gpio );
+        break;
+      default: assert( false );
+    }
     return 0; 
 }
 
-int gpioPWM(unsigned user_gpio, unsigned dutycycle) { 
+int gpioPWM(unsigned gpio, unsigned dutycycle) {
+    gpio_set_function( gpio, GPIO_FUNC_PWM );
+    pwm_set_gpio_level( gpio, dutycycle );
     return 0; 
 }
 
 int gpioSetPWMfrequency(unsigned gpio, unsigned frequency) {
+    pwm_set_wrap( pwm_gpio_to_slice_num( gpio ), 255);
+    pwm_set_enabled( pwm_gpio_to_slice_num( gpio ), true );
     return 0;
 }
 
@@ -63,15 +180,54 @@ int gpioCfgClock(unsigned micros, unsigned peripheral, unsigned source) {
     return 0;
 }
 
-int gpioGlitchFilter(unsigned user_gpio, unsigned steady) {
-    return 0;
+static uint8_t myAddr[2] = {0,};
+
+static i2c_inst_t* i2cHandle(unsigned id) {
+  switch ( id ) {
+    case 0: return( i2c0 );
+    case 1: return( i2c1 );
+    default: assert( false );
+  }
+  return 0;
 }
 
-int i2cWriteByteData(unsigned handle, unsigned i2cReg, unsigned bVal) {
-    return 0; 
+int i2cWriteByteData(unsigned id, unsigned i2cReg, unsigned bVal) {
+    const uint8_t byte = bVal;
+    printf("i2c_write_blocking id:%d handle:%p addr:%x\n", id, i2cHandle(id), myAddr[id]);
+    int ret = i2c_write_blocking(	i2cHandle(id), myAddr[id], &byte, 1/*len*/, false/*nostop*/ );
+    if ( ret < sizeof(byte) ) {
+        assert( ret != PICO_ERROR_GENERIC );
+    }
+    return ret; 
 }
 
-int i2cOpen(unsigned i2cBus, unsigned i2cAddr, unsigned i2cFlags) { 
-    return 0; 
+int i2cOpen(unsigned id, unsigned addr, unsigned i2cFlags) { 
+    const uint baud = 100e3;
+    //const uint baud = 400e3;
+    // const uint baud = 1700e3;
+    int ret = i2c_init(	i2cHandle(id), baud );
+    printf("i2c_init ret:%d id:%d handle:%p addr:%x\n", ret, id, i2cHandle(id), addr); // mux=0x20
+    assert( ret > baud * 0.99 && ret < baud * 1.01 );
+    myAddr[ id ] = addr;
+    
+    uint8_t sda;
+    uint8_t scl;
+    switch ( id ) { 
+      case 0:
+        sda = MCP23017_SDA;   // i2c0_sda
+        scl = MCP23017_SCL;   // i2c0_scl
+        break;
+      case 1:
+        sda = DISPLAY_SDA;   // i2c1_sda
+        scl = DISPLAY_SCL;   // i2c1_scl
+        break;
+      default: assert( false );
+    }
+    gpio_set_function( sda, GPIO_FUNC_I2C );
+    gpio_set_function( scl, GPIO_FUNC_I2C );
+    gpio_pull_up( sda );
+    gpio_pull_up( scl );
+    printf("i2copen:%d\n",id);
+    return id; 
 }
 
--- portable.h
+++ portable.h
@@ -3,38 +3,37 @@
 
 //////////////////////////////////////////////////////////////////////////
 // arbitrary connections of stewie pinball machine to raspberry pi io
-    
-#define SOLENOID_FLIPPER_LEFT    4
-#define SOLENOID_FLIPPER_RIGHT   17
-#define SOLENOID_SHOOTER         27
-#define LAMP_PLAYFIELD           22
-#define LAMP_SHOOT_AGAIN         5    
 
-#define SWITCH_START             10
-#define SWITCH_FLIPPER_LEFT      11
-#define SWITCH_FLIPPER_RIGHT     9    
-#define SWITCH_LOIS              23
-#define SWITCH_MEG               24
-#define SWITCH_PETER             25
-#define SWITCH_SHOOTER           8
-#define SWITCH_BRIAN_CHRIS       12
-#define SWITCH_CHRIS_BRIAN       16    
-#define SWITCH_DRIVE_SIGNAL      7
-    
-#define LED_MATRIX_CLK           6
-#define LED_MATRIX_D0_7          MCP23017_A0_7
+#define SERIAL_TX                0    // uart0_tx
+#define SERIAL_RX                1    // uart0_rx
+#define SWITCH_FLIPPER_LEFT      2
+#define SWITCH_START             3
+#define SWITCH_FLIPPER_RIGHT     4
+#define SWITCH_SHOOTER           5
+#define SOLENOID_FLIPPER_LEFT    6
+#define SOLENOID_FLIPPER_RIGHT   7
+#define SOLENOID_SHOOTER         8
+#define LAMP_PLAYFIELD           9
+#define LAMP_SHOOT_AGAIN         10
+#define LED_MATRIX_CLK           11    // MCP23017
+#define GPIO_UNUSED_12           12
+#define GPIO_UNUSED_13           13
+#define DISPLAY_SDA              14   // i2c1_sda
+#define DISPLAY_SCL              15   // i2c1_scl
+#define GPIO_UNUSED_16           16
+#define SWITCH_CHRIS_BRIAN       17
+#define SWITCH_BRIAN_CHRIS       18
+#define SWITCH_DRIVE_SIGNAL      19
+#define MCP23017_SDA             20   // i2c0_sda
+#define MCP23017_SCL             21   // i2c0_scl
+#define SWITCH_PETER             22
+#define GPIO_NA_23               23
+#define GPIO_NA_24               24
+#define PICO_LED                 25   // built in LED
+#define SWITCH_MEG               26  
+#define SWITCH_LOIS              27
+#define GPIO_UNUSED_28           28
 
-#define MCP23017_SDA             2
-#define MCP23017_SCL             3
-    
-#define DISPLAY_SDA              2
-#define DISPLAY_SCL              3
-    
-#define AUDIO_PCM_FS             19    
-#define AUDIO_PCM_CLK            18    
-#define AUDIO_PCM_DOUT           21    
-
-    
 // https://github.com/joan2937/pigpio/issues/397
 #define gpioCancelTimer(t) gpioSetTimerFunc(t,PI_MIN_MS,0)
 
@@ -54,47 +53,70 @@
     
 ///////////////////////////////////////////////////////////////////////////
 // poorman's hardware abstraction layer 
-// allowing this code to be built on a desktop
-// basically just a pigpio lib that debug outputs to stdio
-// rather than real hardware
+// 
+// basically a pigpio compliant api that allows for all 3 of the following targets:
+//
+// 1. raspberry pi
+// 2. desktop (hardware is just stdio)
+// 3. pi pico
 
-#ifdef RASPBERRY
+
+#if defined( RASPBERRY )
   
-  #include <pigpio.h> // http://abyz.me.uk/rpi/pigpio/
-  
-#else
+    // we're using the real PiGpio C library
+    #include <pigpio.h> // http://abyz.me.uk/rpi/pigpio/
 
-#define PI_MIN_MS    0
-#define PI_INPUT     0
-#define PI_OUTPUT    1
-#define PI_PUD_OFF   0
-#define PI_PUD_DOWN  1
-#define PI_PUD_UP    2
-#define RISING_EDGE  0
-#define FALLING_EDGE 1
-    
 
-typedef void (*gpioTimerFunc_t)    (void);
-typedef void (*gpioAlertFunc_t)    (int gpio, int level, uint32_t tick);
-typedef void *(gpioThreadFunc_t)   (void*);
+#else 
 
-int gpioCfgClock(unsigned micros, unsigned peripheral, unsigned source);
-int gpioInitialise(void);
-void* gpioStartThread(gpioThreadFunc_t f, void *userdata);
-int gpioSetMode(unsigned gpio, unsigned mode);
-int gpioSetTimerFunc(unsigned timer, unsigned millis, gpioTimerFunc_t f);
-int gpioSetAlertFunc(unsigned user_gpio, gpioAlertFunc_t f);
-int gpioGlitchFilter(unsigned user_gpio, unsigned steady);
-int gpioWrite(unsigned gpio, unsigned level);
-int gpioRead(unsigned gpio);
-int gpioSetPullUpDown(unsigned gpio, unsigned pud);
-int gpioPWM(unsigned user_gpio, unsigned dutycycle);
-int gpioSetPWMfrequency(unsigned gpio, unsigned frequency);
-int gpioGetPWMfrequency(unsigned gpio);
-int i2cWriteByteData(unsigned handle, unsigned i2cReg, unsigned bVal);
-int i2cOpen(unsigned i2cBus, unsigned i2cAddr, unsigned i2cFlags);
+    // we're using a local compliant glue layer
+    // desktop or pico
+
+    #if defined( RASPBERRY_PICO )
+
+        #include "pico/stdlib.h" // defs RASPBERRYPI_PICO
+        void haltPico(void);
+
+    #endif
+
+    #define PI_MIN_MS    0
+    #define PI_INPUT     0
+    #define PI_OUTPUT    1
+    #define PI_PUD_OFF   0
+    #define PI_PUD_DOWN  1
+    #define PI_PUD_UP    2
+    #define RISING_EDGE  0
+    #define FALLING_EDGE 1
+        
+
+    typedef void (*gpioTimerFunc_t)    (void);
+    typedef void (*gpioAlertFunc_t)    (int gpio, int level, uint32_t tick);
+    typedef void *(gpioThreadFunc_t)   (void*);
+
+    int gpioCfgClock(unsigned micros, unsigned peripheral, unsigned source);
+    int gpioInitialise(void);
+    void* gpioStartThread(gpioThreadFunc_t f, void *userdata);
+    int gpioSetMode(unsigned gpio, unsigned mode);
+    int gpioSetTimerFunc(unsigned timer, unsigned millis, gpioTimerFunc_t f);
+    int gpioSetAlertFunc(unsigned user_gpio, gpioAlertFunc_t f);
+    int gpioGlitchFilter(unsigned user_gpio, unsigned steady);
+    int gpioWrite(unsigned gpio, unsigned level);
+    int gpioRead(unsigned gpio);
+    int gpioSetPullUpDown(unsigned gpio, unsigned pud);
+    int gpioPWM(unsigned user_gpio, unsigned dutycycle);
+    int gpioSetPWMfrequency(unsigned gpio, unsigned frequency);
+    int gpioGetPWMfrequency(unsigned gpio);
+    int i2cWriteByteData(unsigned handle, unsigned i2cReg, unsigned bVal);
+    int i2cOpen(unsigned i2cBus, unsigned i2cAddr, unsigned i2cFlags);
 
 #endif
 
 #endif // _PORTABLE_H_
+
+// pico sdk's assert leaves something to be desired, so...
+#if defined( RASPBERRY_PICO )
+  #undef assert
+  #define assert(condition) if (!(condition)) { printf("assert %s line %d\n", __FILE__, __LINE__); haltPico(); }
+#endif
+
 
